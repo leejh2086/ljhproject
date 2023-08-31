28c689ca765b849d21fa049a3354c61f42b69ec0
이종훈 <test@gmail.com>
2023-08-31 17:32:44 +0900

    0831

--- src/main/resources/static/js/writeScript.js
+++ src/main/resources/static/js/writeScript.js
@@ -1,6 +1,6 @@
 function set(){
   let editor = document.querySelector(".ql-editor").innerHTML;
   let bContents = document.querySelector("#b_Contents");
-  let submit = document.querySelector("#submit");
+  let select = document.querySelector("#select");
   bContents.value = editor;
 }
